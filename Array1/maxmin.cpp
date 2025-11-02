9d1ed09f4bbb8d3fce08d85bd1c31c692f56357d
RituSingh09 <ritusingh0924@gmail.com>
2025-11-02 22:53:19 +0530

    Delete Array1/maxmin.cpp

--- Array1/maxmin.cpp
+++
@@ -1,5 +0,0 @@
-ement in nums that has the lowest value and an element that has the highest value. We call them the minimum and maximum respectively. Your goal is to remove both these elements from the array.
-
-A deletion is defined as either removing an element from the front of the array or removing an element from the back of the array.
-
-Return the minimum numb
